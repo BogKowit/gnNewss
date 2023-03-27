62fcd9b807c9be59b3ba116e164f8ccaed133bf2
BogKowit <BogKowit@gmail.com>
2023-03-27 13:15:35 +0200

    ++

--- src/functions/divideStringIntoPairs.ts
+++ src/functions/divideStringIntoPairs.ts
@@ -1,12 +1,11 @@
 import iso from "iso-3166-1";
 
-
 export default function divideStringIntoPairs(str: string) {
   const pairs = [];
   for (let i = 0; i < str.length; i += 2) {
     pairs.push(str.slice(i, i + 2));
   }
   const countriesFunc: any[] = [];
-  pairs.map((value) => countriesFunc.push(iso.whereAlpha2(`${value}`)));
+  pairs?.map((value) => countriesFunc.push(iso.whereAlpha2(`${value}`)));
   return countriesFunc;
 }
