fd65081cb6f7e31cdb1d61c59d87051d8741412a
BogKowit <BogKowit@gmail.com>
2023-03-27 02:48:25 +0200

    ++

--- src/utils/fetchDataNewsByCountry.ts
+++ src/utils/fetchDataNewsByCountry.ts
@@ -1,5 +1,6 @@
 const fetchDataNewsByCountry = async (country: string) => {
-  const API_KEY = `${process.env.NEWS_API_KEY}`;
+  const API_KEY = `846c8d71323d43639bb9075abcbaa642
+  `;
   if (country === "" || null || undefined) {
     return (country = "us");
   }
@@ -7,6 +8,7 @@ const fetchDataNewsByCountry = async (country: string) => {
   try {
     const res = await fetch(API_URL);
     const data = await res.json();
+    console.log(data);
     return data;
   } catch (error) {
     console.error(error);
