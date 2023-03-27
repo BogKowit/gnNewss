bc938550f54531a7af86212f27aa754fb351b15a
BogKowit <BogKowit@gmail.com>
2023-03-27 13:53:04 +0200

    ++

---
+++ .netlify/functions/fetchNews.js
@@ -0,0 +1,24 @@
+const fetch = require('node-fetch');
+
+exports.handler = async (event, context) => {
+  const country = event.queryStringParameters.country || 'us';
+  const API_KEY = "846c8d71323d43639bb9075abcbaa642";
+  const API_URL = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
+
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data),
+    };
+  } catch (error) {
+    console.error(error);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: 'Something went wrong!' }),
+    };
+  }
+};
