5f69b0ca64bcb50d0d907ff754d75d2760278cd0
BogKowit <BogKowit@gmail.com>
2023-03-27 15:22:45 +0200

    netlify test

--- src/utils/fetchDataNewsByCountry.ts
+++ src/utils/fetchDataNewsByCountry.ts
@@ -1,28 +1,16 @@
-// const fetchDataNewsByCountry = async (country: string) => {
-//   const API_KEY = "846c8d71323d43639bb9075abcbaa642";
-//   if (country === "" || null || undefined) {
-//     return (country = "us");
-//   }
-//   const API_URL = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
-//   try {
-//     const res = await fetch(API_URL);
-//     const data = await res.json();
-//     console.log(data);
-//     return data;
-//   } catch (error) {
-//     console.error(error);
-//   }
-// };
-
-
 const fetchDataNewsByCountry = async (country: string) => {
+  const API_KEY = "846c8d71323d43639bb9075abcbaa642";
+  if (country === "" || null || undefined) {
+    return (country = "us");
+  }
+  const API_URL = `https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${API_KEY}`;
   try {
-    const res = await fetch(`/.netlify/functions/fetchNews?country=${country}`);
+    const res = await fetch(API_URL);
     const data = await res.json();
-    console.log("data:",data);
+    console.log(data);
     return data;
   } catch (error) {
-    console.log("error",error);
+    console.error(error);
   }
 };
 
